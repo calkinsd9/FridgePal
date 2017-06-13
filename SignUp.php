8845502c8f5196b397244b449217903243e9666c
David Calkins <calkinsd9@yahoo.com>
2017-06-13 16:12:20 -0400

    added form validation to signup

--- SignUp.php
+++ SignUp.php
@@ -20,16 +20,17 @@ if (isset($_SESSION['username'])){
 </head>
 <body>
 <h1>Sign Up</h1>
-<form action="SignUp.php" method="post">
+<form action="SignUp.php" method="post" onsubmit="event.preventDefault(); return validateForm();>
     <label for="username">Username</label>
     <input type="text" name="username" id="username" value="" />
     <p class="warning" id="usernameWarning">* That username has already been taken; please choose another</p>
     <br>
     <label for="password">Password</label>
-    <input type="text" name="password" id="password" value="" /> <br />
+    <input type="text" name="password" id="password" value="" onblur="checkForEmpty()"/> <br />
+    <p class="warning" id="passwordWarning">* Password cannot be blank</p>
     <br>
     <br>
-    <input type="submit" name="submitButton" value="Submit" />
+    <input type="submit" name="submitButton" value="Submit"/>
 </form>
 
 </body>
@@ -52,5 +53,26 @@ if (isset($_SESSION['username'])){
         };
         ajax.send();
     });
+</script>
+<script type="text/javascript">
+    var checkForEmpty = function () {
+        var input = this.value;
+        if (!input){
+            document.getElementById("passwordWarning").style.display = "inherit";
+        }
+        else {
+            document.getElementById("passwordWarning").style.display = "none";
+        }
+    };
+    
+    var validateForm = function () {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        if (!username || !password){
+            alert("username and password must not be blank");
+            return false;
+        }
+        return true;
+    }
 </script>
 </html>
